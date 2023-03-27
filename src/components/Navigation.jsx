a7dc442b76852f2ee03d3f8ee0d3cd6bca43b766
Abrosss <nataroro5@gmail.com>
2023-03-27 20:35:36 +0600

    added Tips button on the quiz page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Bulb from '../assets/bulb.svg'
 function Navigation(props) {
     const { backTo, currentPage, linkTo } = props
     return (
         <nav>
+            <div>
             <Link
                 className='highlighted'
                 to={linkTo}>{backTo}</Link>
             <span> / {currentPage}</span>
+            </div>
+            <div className='tips'>
+                <img className='icon' src={Bulb}></img>
+                
+                <span>Tips</span>
+            </div>
         </nav>
     )
 }
