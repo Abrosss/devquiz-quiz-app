146468c8fb500b094a4ef29abff0781c834a474c
Raj Singh <rajsinghast03@gmail.com>
2023-06-18 09:17:56 +0530

    fix gap

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Restart from '../assets/restart.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import Restart from "../assets/restart.svg";
 function Result(props) {
   const { correctAnswerCount, amountOfQuestions, handleRestartQuiz } = props;
   return (
     <div className="container-content">
       <h2>You're finished!</h2>
-      <p>Result: {correctAnswerCount}/{amountOfQuestions}</p>
-      <div>
-       
-        <Link to="/" className='button'>Back To Tests</Link>
-        <button className='button no-background buttonWithIcon' onClick={handleRestartQuiz}>
-          <img className='icon' src={Restart} alt="restart"></img>
+      <p>
+        Result: {correctAnswerCount}/{amountOfQuestions}
+      </p>
+      <div className="result">
+        <Link to="/" className="button btn">
+          Back To Tests
+        </Link>
+        <button
+          className="button no-background buttonWithIcon"
+          onClick={handleRestartQuiz}
+        >
+          <img className="icon" src={Restart} alt="restart"></img>
           <span>Restart</span>
         </button>
-        </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Result
+export default Result;
