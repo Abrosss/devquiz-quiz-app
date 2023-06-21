f870c5047b10c98579f67fdf3dffbcda4f1eb21c
Jhonel Villanueva <jonichi16@gmail.com>
2023-06-21 18:23:36 +0800

    added github repo link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
-function Header({position}) {
+function Header({ position }) {
   return (
     <footer className={position}>
-        <Link to="/categories">
-            <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-          </Link>
+      <a href='https://github.com/Abrosss/devquiz-quiz-app' target='_blank'>
+        <img
+          className='icon'
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'
+        />
+      </a>
     </footer>
-  )
+  );
 }
 
-export default Header
+export default Header;
