1bd510f382899932c6536cbb3e63769441a61705
Abrosss <nataroro5@gmail.com>
2023-07-01 17:12:04 +0600

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import path from 'path'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
-const viteEnv = {}
+const viteEnv = {};
 Object.keys(process.env).forEach((key) => {
   if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
   }
-})
-
-export default {
+});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   alias: {
-    '@': require('path').resolve(__dirname, 'src')
+    '@': path.resolve(__dirname, 'src'),
   },
-  define: viteEnv,
-  plugins: [react()]
-}
+  define: viteEnv
+})
